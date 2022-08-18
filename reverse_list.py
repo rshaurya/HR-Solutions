01b10d4b09f72abdb28eb253eb637fb0923c3de1
Shaurya Pethe <rajesh.pethe+shaurya@gmail.com>
2022-08-18 20:20:38 +0530

    small changes

--- reverse_list.py
+++ reverse_list.py
@@ -1,10 +1,14 @@
 # https://www.hackerrank.com/challenges/30-arrays/problem?h_r=profile
 # return the reverse of a list in the form of space separated integers
 
-def rev_list(arr):
+# with reverse function
+def list_reverse(arr):
     arr.reverse()
     for i in arr:
         print(i, end=" ")
 
-print(rev_list([1,2,3,4,5]))
+print(list_reverse([1,2,3,4,5]))
+
+
+
 
